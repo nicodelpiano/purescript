6937e30a13ae45987d495ff46c9eada9721de733
Phil Freeman <paf31@cantab.net>
2015-06-10 12:57:22 -0700

    Basic framework for exhaustivity checking

--- src/Language/PureScript/Linter/Exhaustive.hs
+++ src/Language/PureScript/Linter/Exhaustive.hs
@@ -10,10 +10,18 @@
 --
 -----------------------------------------------------------------------------
 
-module Language.PureScript.Linter.Exhaustive where
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Language.PureScript.Linter.Exhaustive 
+  ( checkExhaustive
+  , checkExhaustiveModule
+  ) where
 
 import qualified Data.Map as M
-import Data.Maybe (fromMaybe)
+
+import Control.Applicative
+import Control.Monad.Writer.Class
 
 import Language.PureScript.AST.Binders
 import Language.PureScript.AST.Declarations
@@ -21,6 +29,9 @@ import Language.PureScript.Environment
 import Language.PureScript.Names as P
 import Language.PureScript.Kinds
 import Language.PureScript.Types as P
+import Language.PureScript.Errors
+
+import Language.PureScript.AST.Traversals (everywhereOnValuesTopDownM)
 
 -- |
 -- | Constructors handling
@@ -42,57 +53,81 @@ getConstructors env n = go lnte
   go _ = []
 
 -- |
---
---
-zipMissing :: Environment -> [Binder] -> [Binder] -> [[Binder]]
-zipMissing env bs bs' = map (uncurry $ missingCasesSingle env) $ zip bs bs'
-
--- |
 -- Find the uncovered set between two binders:
 -- the first binder is the case we are trying to cover
 -- the second one is the matching binder
 --
 missingCasesSingle :: Environment -> Binder -> Binder -> [Binder]
-
--- any-wildcard
 missingCasesSingle _ _ NullBinder = []
-
--- any-var
 missingCasesSingle _ _ (VarBinder _) = []
-
--- cons-(wildcard|var)
+missingCasesSingle _ b _ = [b]
+{-
 missingCasesSingle env (ConstructorBinder con bs) b =
   case b of
     NullBinder -> undefined
     VarBinder _ -> undefined
     _ -> undefined
-
 -- cons-cons
 -- ConstructorBinder (Qualified ProperName) [Binder]
 missingCasesSingle env cb@(ConstructorBinder con bs) (ConstructorBinder con' bs')
   | con == con' = undefined 
   | otherwise = [cb]
-
--- undef
-missingCasesSingle _ _ _ = undefined
+-}
 
 -- |
 -- Returns the uncovered set of binders
 -- the first argument is the list of uncovered binders
 -- the second argument is a clause of a pattern matching def
 --
-missingCasesMultiple :: [Binder] -> [Binder] -> [[Binder]]
-missingCasesMultiple = undefined
+missingCasesMultiple :: Environment -> [Binder] -> [Binder] -> [[Binder]]
+missingCasesMultiple env = go
+  where
+  go [] _ = []
+  go (x:xs) (y:ys) 
+    | null miss = map (x :) (go xs ys)
+    | otherwise = map (: xs) miss ++ map (x :) (go xs ys)
+    where
+    miss = missingCasesSingle env x y
+  go _ _ = error "Argument lengths did not match in missingCasesMultiple."
 
 -- |
 -- Returns the uncovered set of case alternatives
---
-missingCases :: CaseAlternative -> CaseAlternative -> [CaseAlternative]
-missingCases = undefined
+-- TODO: handle guards
+missingCases :: Environment -> [Binder] -> CaseAlternative -> [[Binder]]
+missingCases env uncovered ca = missingCasesMultiple env uncovered (caseAlternativeBinders ca)
 
 -- |
 -- Main exhaustivity checker function
 -- Returns the uncovered set of case alternatives.
 -- 
-checkExhaustive :: Environment -> [CaseAlternative] -> [CaseAlternative] -> [[CaseAlternative]]
-checkExhaustive = undefined
+checkExhaustive :: forall m. (MonadWriter MultipleErrors m) => Environment -> [CaseAlternative] -> m ()
+checkExhaustive env cas = makeResult $ foldl step [initial] cas
+  where
+  step :: [[Binder]] -> CaseAlternative -> [[Binder]]
+  step uncovered ca = concatMap (\u -> missingCases env u ca) uncovered
+
+  initial :: [Binder]
+  initial = replicate numArgs NullBinder
+    where
+    numArgs = length . caseAlternativeBinders . head $ cas 
+
+  makeResult :: [[Binder]] -> m ()
+  makeResult bss | null bss = return ()
+                 | otherwise = tell (error "TODO: add new warning type")
+
+checkExhaustiveModule :: forall m. (Applicative m, MonadWriter MultipleErrors m) => Environment -> Module -> m ()
+checkExhaustiveModule env (Module _ _ ds _) = 
+  let (f, _, _) = everywhereOnValuesTopDownM return checkExpr return
+  in mapM_ f ds
+  where
+  checkExpr :: Expr -> m Expr
+  checkExpr c@(Case _ cas) = checkExhaustive env cas >> return c
+  checkExpr other = return other
+
+
+
+
+
+
+
+
